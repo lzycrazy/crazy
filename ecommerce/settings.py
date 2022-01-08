af392a515b31ca489dee3faaa91ca2d4ede861d7
Jahid Hasan <lzycrazy1@gmail.com>
2022-01-08 14:35:34 +0530

    lkkk
    Signed-off-by: Jahid Hasan <lzycrazy1@gmail.com>

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70si&)q_&fsp_o-wi+2a^muik^!&c4re2)7n0$(@tju@o5o1wj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['143.198.126.130']
 
